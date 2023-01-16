b8bf40a1a008add0c0afa75e90c4296e5b2becfc
defalt926 <defalt926@github.com>
2023-01-16 19:41:13 +0100

    add categories to homepage

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,15 @@
+import { Category } from "./category.module"
+
+export const CONST = {
+    categories: [
+        new Category("1", "Food & Drink"),
+        new Category("2", "Electronics"),
+        new Category("3", "Home & Garden"),
+        new Category("4", "Sporting Goods")
+    ],
+    subCategories: [
+        
+    ]
+}
+
+
