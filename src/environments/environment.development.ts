59a831f0a366d39cda5bf236a795aaaf89307a03
defalt926 <defalt926@github.com>
2023-01-26 21:46:52 +0100

    firebase hosting

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+    firebaseConfig : {
+        apiKey: "AIzaSyCCM6Tx0vHaNxBsofb9qc1d4Y8bNTJai3M",
+        authDomain: "goods-searcher.firebaseapp.com",
+        projectId: "goods-searcher",
+        storageBucket: "goods-searcher.appspot.com",
+        messagingSenderId: "162461017067",
+        appId: "1:162461017067:web:fa964b91f3afc101ea2fae",
+        measurementId: "G-Y9NMCYFZQ9"
+    },
+    production: false
+};
