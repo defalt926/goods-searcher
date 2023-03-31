b03d39af06ee56ca8307d448a93061bf46ba9c27
defalt926 <defalt926@github.com>
2023-03-31 18:03:16 +0200

    add shops page

--- src/app/pages/shops/shops.component.ts
+++ src/app/pages/shops/shops.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ShopService } from 'src/app/services/shop.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { CONST } from 'src/app/shared/constants';
 import { Shop } from 'src/app/shared/models/shop.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
+import { AddShopDialogComponent } from './add-shop-dialog/add-shop-dialog.component';
 
 @Component({
   selector: 'app-shops',
@@ -9,14 +16,50 @@ import { Shop } from 'src/app/shared/models/shop.model';
   styleUrls: ['./shops.component.css']
 })
 export class ShopsComponent {
+  authService: AuthService = {} as AuthService;
   shop: Shop = {} as Shop;
+  displayedColumns: string[] = ['id', 'name', 'city', 'street'];
+  dataSource = new MatTableDataSource([] as Shop[]);
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
     private route: ActivatedRoute,
-    private service: ShopService,
-  ) {}
+    private shopService: ShopService,
+    authService: AuthService,
+    private _liveAnnouncer: LiveAnnouncer,
+    private router: Router,
+    public dialog: MatDialog
+  ) {
+    this.authService = authService;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 
   ngOnInit() {
-    this.shop = this.service.getShopById(this.route.snapshot.paramMap.get('shop_id'));
+    var currentShopId = this.route.snapshot.paramMap.get('shop_id');
+    this.shop = this.shopService.getShopById(currentShopId);
+    this.dataSource = new MatTableDataSource(CONST.shops);
+  }
+
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  goToShopDetails(shop_id: string) {
+    this.router.navigateByUrl('shop/' + shop_id);
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(AddShopDialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.dataSource = new MatTableDataSource(CONST.shops);
+    });
   }
 }
--- src/app/pages/shops/shops.module.ts
+++ src/app/pages/shops/shops.module.ts
@@ -3,17 +3,35 @@ import { CommonModule } from '@angular/common';
 import { ShopsRoutingModule } from './shops-routing.module';
 import { ShopsComponent } from './shops.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AddShopDialogComponent } from './add-shop-dialog/add-shop-dialog.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    ShopsComponent
+    ShopsComponent,
+    AddShopDialogComponent
   ],
   imports: [
     CommonModule,
     ShopsRoutingModule,
-    MatCardModule
+    MatCardModule,
+    MatTableModule,
+    MatSortModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ]
 })
 export class ShopsModule { }
