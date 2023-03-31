8dcb91f12d9bd88c9f0b09adad7c5fb31fe16f37
defalt926 <defalt926@github.com>
2023-03-31 18:07:49 +0200

    add localstorage base authentication

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { CONST } from '../shared/constants';
-import { User } from '../shared/models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-    private user?: User;
-
     constructor(private router: Router) {
     }
 
     login(email: string, password: string) {
-      var user = CONST.users.find(user => user.email === email && user.password === password)
+      var user = CONST.users.find(user => user.email === email
+                                && user.password === password
+                                && !user.is_suspended)
       if (user) {
-        this.user = user; 
+        localStorage.setItem('userId', user.id);
         this.router.navigateByUrl('home');
         return true;
       } else {
@@ -24,6 +23,23 @@ export class AuthService {
     }
 
     getAuthStatus() {
-      return this.user !== undefined;
+      return CONST.users.find(user => user.id == localStorage.getItem('userId')) != null;
+    }
+
+    logout() {
+      localStorage.removeItem('userId');
+      this.router.navigateByUrl('home');
+    }
+
+    getUser() {
+      return CONST.users.find(user => user.id == localStorage.getItem('userId'));
+    }
+
+    getUserName() {
+      return this.getUser()?.last_name + " " + this.getUser()?.first_name;
+    }
+
+    getUserEmail() {
+      return this.getUser()?.email;
     }
 }
