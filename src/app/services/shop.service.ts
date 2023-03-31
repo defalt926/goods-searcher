c64c05f28566542cfe1df69f6274c473f8369c16
defalt926 <defalt926@github.com>
2023-03-31 18:04:52 +0200

    add shop page

--- src/app/services/shop.service.ts
+++ src/app/services/shop.service.ts
@@ -12,4 +12,8 @@ export class ShopService {
   getShopById(shop_id: string | null): Shop {
     return CONST.shops.filter(shop => shop.id == shop_id)[0];
   }
+
+  getItemPriceByIds(shop_id: string, item_id: string): number {
+    return CONST.prices.filter(item => item.shop_id == shop_id && item.item_id == item_id)[0].price;
+  }
 }
